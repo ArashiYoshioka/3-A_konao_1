f86411b44724ca34330e9e6a0234245cf98217ae
ArashiYoshioka <220808a34@hokkaido-c.ed.jp>
2024-07-05 14:58:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ function checkAnswer() {
     const answerInput = document.getElementById('answerInput').value;
     const result = document.getElementById('result');
 
-    if (answerInput === "味噌") {
+    if (answerInput === "みそ") {
+        result.textContent = "正解！";
+    } else if (answerInput === "味噌") {
         result.textContent = "正解！";
     } else {
         result.textContent = "残念...";
