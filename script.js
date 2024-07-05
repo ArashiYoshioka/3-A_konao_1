356539a638d3391005e3a1d04439747b8261ee2b
ArashiYoshioka <220808a34@hokkaido-c.ed.jp>
2024-07-05 14:44:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function checkAnswer() {
     const answerInput = document.getElementById('answerInput').value;
     const result = document.getElementById('result');
 
-    if (answerInput === "みそ") {
+    if (answerInput === "味噌") {
         result.textContent = "正解！";
     } else {
         result.textContent = "残念...";
